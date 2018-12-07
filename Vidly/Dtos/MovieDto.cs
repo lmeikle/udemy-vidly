7cab67cd605842d7fd662de1504a27b5d51b1184
Laura Meikle <laura.s.meikle@gmail.com>
2018-12-07 12:21:02 +0000

    finish section 6

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public byte GenreTypeId { get; set; }
+
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        public DateTime? DateAdded { get; set; }
+
+        [Required]
+        public byte NumberInStock { get; set; }
+    }
+}
